d4316dbb53229d6c83b608bacd183c4abbfe7842
abhinab <abhinabhandique20@gmail.com>
2024-09-05 03:01:20 +0530

    frontend completed

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import About from '../components/About'
 import HeroSection from '../components/HeroSection'
+import Menu from '../components/Menu'
+import Qualities from '../components/Qualities'
+import Reservation from '../components/Reservation'
+import Team from '../components/team'
+import WhoAreWe from '../components/WhoAreWE'
 function Home() {
   return (
     <div>
       <HeroSection></HeroSection>
+      <About></About>
+      <Qualities></Qualities>
+      <Menu></Menu>
+      <WhoAreWe></WhoAreWe>
+      <Team></Team>
+      <Reservation></Reservation>
       
     </div>
   )
